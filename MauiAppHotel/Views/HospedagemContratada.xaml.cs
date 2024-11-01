c9bd3ccf17eff57afdcd000af567961a591d926a
AnClockup <varjao2003@yahoo.com.br>
2024-11-01 18:50:34 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -0,0 +1,33 @@
+namespace MauiAppHotel.Views;
+
+public partial class HospedagemContratada : ContentPage
+{
+	public HospedagemContratada()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PopAsync();
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new Sobre());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
+
+}
